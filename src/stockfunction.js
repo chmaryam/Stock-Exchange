bfa4c380b2bee7244b8d7fe57fab239ecf355bad
maryam23 <emohammad273@gmail.com>
2023-04-08 21:30:43 +0300

    day1

--- src/stockfunction.js
+++ src/stockfunction.js
@@ -1,27 +1,4 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
 
-
-export default function App() {
-    const [data, setData] = useState([]);
-  
-    useEffect(() => {
-      fetch('https://stock-and-options-trading-data-provider.p.rapidapi.com/options/aapl')
-      // headers: {
-        // 'X-RapidAPI-Proxy-Secret': 'a755b180-f5a9-11e9-9f69-7bf51e845926',
-        // 'X-RapidAPI-Key': '8884dfbf43msheb9d5784071dd1ap10662cjsnfd2c548117c2',
-        // 'X-RapidAPI-Host': 'stock-and-options-trading-data-provider.p.rapidapi.com'})
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.log(error))
-    }, []);
-  
-    return (
-        <div>
-        <h1>API Data</h1>
-      </div>
-    );
-  }
   
  
   
