2e44271063a595d04ae3c96547a5de75347821f4
maryam23 <emohammad273@gmail.com>
2023-04-08 21:18:40 +0300

    day1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('OHbh8cmQljwyWSw8wGwTSSrCKcHcKt8h')
+    // headers: {
+      // 'X-RapidAPI-Proxy-Secret': 'a755b180-f5a9-11e9-9f69-7bf51e845926',
+      // 'X-RapidAPI-Key': '8884dfbf43msheb9d5784071dd1ap10662cjsnfd2c548117c2',
+      // 'X-RapidAPI-Host': 'stock-and-options-trading-data-provider.p.rapidapi.com'})
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error))
+  }, []);
+
   return (
-    <div>stock
-
+    <div>
+      <h1>API Data</h1>
+      
     </div>
-  )
+  );
 }
+
+export default App;
